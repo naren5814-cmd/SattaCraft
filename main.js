15d4f792e916b3231dcb063086730f51bb6f16b1
Narendran <naren5814@gmail.com>
2026-06-19 11:33:34 +0530

    Fixed navbar issue

--- main.js
+++ main.js
@@ -67,24 +67,6 @@ hamburger.addEventListener("click", () => {
 
     hamburger.classList.toggle("active");
 
-});
-
-document
-.querySelectorAll(".nav-links a")
-.forEach(link => {
-
-    link.addEventListener("click", () => {
-
-        navLinks.classList.remove("active");
-
-        hamburger.classList.remove("active");
-
-    });
-
-});
-
-hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-
     document.body.classList.toggle("menu-open");
+
 });
